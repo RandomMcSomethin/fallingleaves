0ba02b3f1aeb2d95658e7b0280d1e8178e43c876
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2022-05-05 03:09:16 +0200

    reenable Fabric Seasons integration

--- src/main/java/randommcsomethin/fallingleaves/util/LeafUtil.java
+++ src/main/java/randommcsomethin/fallingleaves/util/LeafUtil.java
@@ -1,8 +1,8 @@
 package randommcsomethin.fallingleaves.util;
 
-//import io.github.lucaargolo.seasons.FabricSeasons;
-//import io.github.lucaargolo.seasons.utils.Season;
-//import net.fabricmc.loader.api.FabricLoader;
+import io.github.lucaargolo.seasons.FabricSeasons;
+import io.github.lucaargolo.seasons.utils.Season;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LeavesBlock;
@@ -45,15 +45,15 @@ public class LeafUtil {
     public static double getModifiedSpawnChance(BlockState state, LeafSettingsEntry leafSettings) {
         double spawnChance = leafSettings.getSpawnChance();
 
-//        if (FabricLoader.getInstance().isModLoaded("seasons")) {
-//            if (FabricSeasons.getCurrentSeason() == Season.FALL) {
-//                // TODO this is a bit weird because some trees like Traverse's autumnal leaves already have boosted values
-//                // double autumn, what does it mean?
-//                spawnChance *= CONFIG.fallSpawnRateFactor;
-//            } else if (FabricSeasons.getCurrentSeason() == Season.WINTER) {
-//                spawnChance *= CONFIG.winterSpawnRateFactor;
-//            }
-//        }
+        if (FabricLoader.getInstance().isModLoaded("seasons")) {
+            if (FabricSeasons.getCurrentSeason() == Season.FALL) {
+                // TODO this is a bit weird because some trees like Traverse's autumnal leaves already have boosted values
+                // double autumn, what does it mean?
+                spawnChance *= CONFIG.fallSpawnRateFactor;
+            } else if (FabricSeasons.getCurrentSeason() == Season.WINTER) {
+                spawnChance *= CONFIG.winterSpawnRateFactor;
+            }
+        }
 
         if (CONFIG.decaySpawnRateFactor != 1.0f) {
             if (isLeafBlock(state.getBlock(), true) && state.getBlock().hasRandomTicks(state)) { // decaying leaves have random ticks
