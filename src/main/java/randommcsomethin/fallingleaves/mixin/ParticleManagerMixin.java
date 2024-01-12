d864c8ceb5f9d3765ec0579b7a9afeb1a176ad88
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2024-01-12 10:39:39 +0100

    spawn particles without registering them

--- src/main/java/randommcsomethin/fallingleaves/mixin/ParticleManagerMixin.java
+++ src/main/java/randommcsomethin/fallingleaves/mixin/ParticleManagerMixin.java
@@ -4,14 +4,22 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import randommcsomethin.fallingleaves.init.Leaves;
+import randommcsomethin.fallingleaves.particle.FallingLeafParticle;
 import randommcsomethin.fallingleaves.util.Wind;
 
+import java.util.Map;
+
 import static randommcsomethin.fallingleaves.init.Config.CONFIG;
+import static randommcsomethin.fallingleaves.init.Leaves.FACTORIES;
+import static randommcsomethin.fallingleaves.init.Leaves.IDS;
 
 @Environment(EnvType.CLIENT)
 @Mixin(ParticleManager.class)
@@ -19,6 +27,20 @@ public abstract class ParticleManagerMixin {
 
     @Shadow
     protected ClientWorld world;
+
+    @Shadow @Final
+    private Map<Identifier, ParticleManager.SimpleSpriteProvider> spriteAwareFactories;
+
+    @Inject(method = "registerDefaultFactories", at = @At("RETURN"))
+    public void registerLeafFactories(CallbackInfo ci) {
+        for (var type : Leaves.TYPES) {
+            Identifier id = IDS.get(type);
+
+            var spriteProvider = SimpleSpriteProviderInvoker.init();
+            spriteAwareFactories.put(id, spriteProvider);
+            FACTORIES.put(type, new FallingLeafParticle.BlockStateFactory(spriteProvider));
+        }
+    }
 
     @Inject(method = "tick", at = @At("HEAD"))
     public void tickWind(CallbackInfo ci) {
